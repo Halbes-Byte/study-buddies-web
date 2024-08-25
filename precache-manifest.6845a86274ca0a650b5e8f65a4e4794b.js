f792fcd759f70b7b3f22961a6d8eaf49ba1bf8d6
alex <alexandra.zwinger@kurzdigital.com>
2024-08-25 11:17:14 +0200

    Updates

---
+++ precache-manifest.6845a86274ca0a650b5e8f65a4e4794b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36464fe5dcd12005a778f343a7f508a5",
+    "url": "/Study-Buddies-Web/index.html"
+  },
+  {
+    "revision": "b1044fa478e3d9896b74",
+    "url": "/Study-Buddies-Web/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/Study-Buddies-Web/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/Study-Buddies-Web/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "b1044fa478e3d9896b74",
+    "url": "/Study-Buddies-Web/static/js/main.704f432d.chunk.js"
+  },
+  {
+    "revision": "62d777a3112bbc975a08",
+    "url": "/Study-Buddies-Web/static/js/runtime~main.926f9992.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Study-Buddies-Web/static/media/logo.06e73328.svg"
+  }
+]);
