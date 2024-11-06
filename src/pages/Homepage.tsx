25642fcb5bd4257bccb2a49b0b4f4cf79a7f6114
alex <alexandra.zwinger@kurzdigital.com>
2024-11-06 14:38:47 +0100

    Add logo

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,8 +1,17 @@
 import '../index.css';
+import logo from '../data//halbesByte.jpeg';
+import React from "react";
+import {Link} from "react-router-dom";
+
 export default function Homepage() {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <h1 className="text-7xl text-gray-300">Study-Buddies</h1>
+        <div className="relative">
+            <Link to="">
+                <img className="absolute top-20 left-20 z-60 h-10" src={logo} alt={"back to homepage"}/>
+            </Link>
+            <div className="flex justify-center items-center h-screen">
+                <h1 className="text-7xl text-gray-300">Study-Buddies</h1>
+            </div>
         </div>
     );
 }
