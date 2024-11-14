39d1707cfac467ea773c7dbb8c1e4fa9d6431688
kerry55 <kerryc417@gmail.com>
2024-11-14 15:14:21 +0100

    hab 1 hübsches menü gemacht, das nichts kann

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,6 +1,6 @@
 import '../index.css';
 import logo from '../data//halbesByte.jpeg';
-import pinguin from '../data//penguin-48226_1280.svg';
+import titelbild from '../data//titelbild.png';
 import menu from '../data//menu-button.svg';
 import React from "react";
 import {Link} from "react-router-dom";
@@ -9,23 +9,39 @@ import {Link} from "react-router-dom";
 
 export function Navbar() {
     return (
-      <nav className=" bg-[#111111] text-white p-4">
-        <div className="flex justify-between items-center">
-          {/* Logo als Link */}
+      <nav className="bg-[#151821] text-white py-7 h-[80px]"> 
+        <div className="flex justify-between items-center relative">
+          
           <Link to="">
-            <img className="absolute top-20 left-60 z-50 h-8" src={menu} alt="Back to homepage" />
+            <img className="absolute top-4 left-4 z-50 h-8" src={menu} alt="Back to homepage" /> 
           </Link>
   
-          {/* Menü Links */}
-          <div className="flex space-x-4">
-            <Link to="" className="text-white hover:text-gray-300">Kalender</Link>
-            {/* Füge hier weitere Links hinzu */}
+         
+          <div className="flex justify-right w-auto mr-[200px]"> 
+            <Link to="" className="text-l text-gray-400 hover:text-[#56A095] hover:font-bold px-5">
+              Home
+            </Link>
+
+            <p className="text-[#56A095]">|</p>
+            <Link to="" className="text-l text-gray-400 hover:text-[#56A095] hover:font-bold px-5">
+              Kalender
+            </Link>
+            <p className="text-[#56A095]">|</p>
+
+            <Link to="" className="text-l text-gray-400 hover:text-[#56A095] hover:font-bold px-5">
+              Aktuelles
+            </Link>
+            <p className="text-[#56A095]">|</p>
+
+            <Link to="" className="text-l text-gray-400 hover:text-[#56A095] hover:font-bold px-5">
+              Score
+            </Link>
           </div>
         </div>
       </nav>
     );
-  }
-  
+}
+
 
 export default function Homepage() {
     return (
@@ -39,16 +55,16 @@ export default function Homepage() {
 
                 <div className="flex flex-row justify-between gap-10">
                     <h1 className="text-7xl font-bold text-gray-300">Study-Buddies</h1>
-                    <img className="h-20 transform scale-x-[-1]" src={pinguin} alt={"ein Pinguin"}/>
+                    <img className="h-20 transform scale-x-[-1]" src={titelbild} alt={"titelbild"}/>
                 </div>
                 <p className="text-xl font-medium text-gray-300">Finde deinen Study-Buddy für deine Study-Party</p>
                 
 
                 <div className="px-0 py-0 flex flex-row justify-start gap-7 h-12"> {/* Kleinere Höhe für die div */}
-                 <button className="px-5 py-1 bg-[#598BB1] text-[#4BC2FE] rounded-full"> {/* Hintergrundfarbe mit Tailwind */}
+                 <button className="px-5 py-1 bg-[#598BB1] text-[#4BC2FE] rounded-full transform transition-all duration-300 hover:scale-x-110"> {/* Hintergrundfarbe mit Tailwind */}
     Erstelle einen Termin
                  </button>
-                <button className="px-5 py-1 bg-[#56A095] text-[#2EF6D9] rounded-full"> {/* Hintergrundfarbe mit Tailwind */}
+                <button className="px-5 py-1 bg-[#56A095] text-[#2EF6D9] rounded-full transform transition-all duration-300 hover:scale-x-110"> {/* Hintergrundfarbe mit Tailwind */}
     Finde einen Lernpartner
              </button>
 </div>
