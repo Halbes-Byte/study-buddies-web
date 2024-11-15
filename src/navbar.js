1999c9fcf735a01561dd310b2d68401c37008d0c
Alexandra Zwinger <alexandra.zwinger@kurzdigital.com>
2024-11-15 12:50:43 +0100

    Format homepage

--- src/navbar.js
+++ src/navbar.js
@@ -3,27 +3,29 @@ import './navbar.css';
 import pinguin from './data/penguin-48226_1280.svg';
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <img className="picture" src={pinguin} alt="Pinguin" />
-      </div>
+    return (
+        <nav className="navbar">
+            <div className="flex justify-self-start">
+                <a href="/">
+                    <img className="picture" src={pinguin} alt="Pinguin"/>
+                </a>
+            </div>
 
-      <div className="navbar-right">
-        <div className="navbar-flex">
-          <div className="navbar-links">
-            <a href="/" className="navbar-link">Home</a>
-            <span className="navbar-separator">|</span>
-            <a href="/calendar" className="navbar-link">Kalender</a>
-            <span className="navbar-separator">|</span>
-            <a href="/score" className="navbar-link">Score</a>
-            <span className="navbar-separator">|</span>
-            <a href="/current" className="navbar-link">Aktuelles</a>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+            <div className="navbar-right">
+                <div className="navbar-flex">
+                    <div className="navbar-links">
+                        <a href="/" className="navbar-link">Home</a>
+                        <span className="navbar-separator">|</span>
+                        <a href="/calendar" className="navbar-link">Kalender</a>
+                        <span className="navbar-separator">|</span>
+                        <a href="/score" className="navbar-link">Score</a>
+                        <span className="navbar-separator">|</span>
+                        <a href="/current" className="navbar-link">Aktuelles</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -4,23 +4,29 @@ import React from "react";
 
 export default function Homepage() {
     return (
-        <div className="relative">
-            <div className="flex justify-center items-center h-screen ">
-                <div className="grid row grid-rows-3 gap-0">
-                    <div className="flex flex-row justify-between gap-10">
+        <div className=" h-screen relative items-center justify-center justify-items-center grid">
+            <div className="h-[70%] relative self-start grid justify-items-center">
+                <div className="grid grid-cols-4 gap-1 justify-self-end">
+
+                    <div className="flex flex-col gap-3 justify-self-end grid-cols-subgrid  col-span-2 self-center">
                         <h1 className="text-7xl font-bold text-gray-300">Study-Buddies</h1>
-                        <img className="h-20 transform scale-x-[-1]" src={title_img} alt={"titelbild"}/>
+                        <p className="text-xl font-medium text-gray-300">Finde deinen Study-Buddy für deine
+                            Study-Party</p>
+                        <div className="px-0 py-0 flex flex-row justify-start gap-7 h-12">
+                            <button
+                                className="px-5 py-1 bg-[#598BB1] text-[#4BC2FE] rounded-full transform transition-all duration-300 hover:scale-x-110"> {/* Hintergrundfarbe mit Tailwind */}
+                                Erstelle einen Termin
+                            </button>
+                            <button
+                                className="px-5 py-1 bg-[#56A095] text-[#2EF6D9] rounded-full transform transition-all duration-300 hover:scale-x-110"> {/* Hintergrundfarbe mit Tailwind */}
+                                Finde einen Lernpartner
+                            </button>
+                        </div>
                     </div>
-                    <p className="text-xl font-medium text-gray-300">Finde deinen Study-Buddy für deine Study-Party</p>
-                    <div className="px-0 py-0 flex flex-row justify-start gap-7 h-12"> {/* Kleinere Höhe für die div */}
-                        <button
-                            className="px-5 py-1 bg-[#598BB1] text-[#4BC2FE] rounded-full transform transition-all duration-300 hover:scale-x-110"> {/* Hintergrundfarbe mit Tailwind */}
-                            Erstelle einen Termin
-                        </button>
-                        <button
-                            className="px-5 py-1 bg-[#56A095] text-[#2EF6D9] rounded-full transform transition-all duration-300 hover:scale-x-110"> {/* Hintergrundfarbe mit Tailwind */}
-                            Finde einen Lernpartner
-                        </button>
+
+                    <div className="w-[150%] self-center justify-self-start">
+
+                        <img className="object-cover overflow-visible justify-self-start self-center" src={title_img} alt={"titelbild"}/>
                     </div>
                 </div>
             </div>
