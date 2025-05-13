bab501b8ea505ecb9ac6a28e2dc618020e40935a
Alexandra Zwinger <alexandra.zwinger@kurzdigital.com>
2025-05-13 10:17:40 +0200

    Finish logout function

--- src/components/navbar/NavbarButtons.tsx
+++ src/components/navbar/NavbarButtons.tsx
@@ -1,39 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import "../../styles/navbar.css";
 import {NavbarComponent} from "./NavbarComponent";
 import {useLocation} from "react-router-dom";
 import LogoutIcon from "@mui/icons-material/Logout";
 import {IconButton} from "@mui/material";
-import axiosInstance from "../../AxiosConfig";
-import {getUser} from "../../api/UserApi";
-import {UserDto} from "../../dtos/UserDto";
 import keycloak from "../../Keycloak";
 
 export const NavbarButtons = () => {
-    const axios = axiosInstance;
-    const [user, setUser] = useState<UserDto>();
-
-    const fetchUser = async () => {
-        const userData = await getUser(axios);
-        setUser(userData);
-    };
-
-    useEffect(() => {
-        fetchUser();
-    }, [])
-
     const logout = () => {
-        fetchUser()
-        if (user?.uuid) {
-            const keycloakUrl = `http://localhost:7070/realms/study-buddies/users/${user?.uuid}/logout`;
-            axios.post(keycloakUrl, {}, {
-                headers: {
-                    Authorization: `Bearer ${keycloak.token}`,
-                }
-            });
-        } else {
-            console.error("Digga, userId ist undefined.");
-        }
+        keycloak.logout({
+            redirectUri: 'http://localhost:3000'
+        });
     };
 
     return (
